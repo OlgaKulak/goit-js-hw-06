f975e54c4eb56e13bf31db60c4e3ce61608e9a36
OlgaKulak <Im.olgakulak@gmail.com>
2022-11-22 17:55:38 +0100

    adds solutions of task #2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ulList = document.getElementById("ingredients");
+for (const item of ingredients) {
+  const li = document.createElement("li");
+  li.textContent = item;
+  ulList.append(li);
+};
+
+console.log(ulList);
